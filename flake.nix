f63c6ee06a71becc31a9097da3808f2f2fbcd042
Naser Aleisa <aleisa.naser@gmail.com>
2023-11-14 03:54:06 +0300

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Dev environment";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells = {
+        default = pkgs.mkShell {
+          nativeBuildInputs = [ pkgs.bashInteractive ];
+          packages = [
+            pkgs.nodePackages.pnpm
+            pkgs.nodejs_20
+          ];
+        };
+      };
+    });
+}
